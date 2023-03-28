319f2b1c49324deaea3710a283de5dd1cd1e0a6e
Xinmeng-Luan <luanxm0903@gmail.com>
2023-03-28 22:42:43 +0200

    I'm in my branch ahhahahahhha

--- Homework 1/HW1.m
+++ Homework 1/HW1.m
@@ -1,3 +1,4 @@
+
 clc;
 clear all;
 close all;
@@ -11,104 +12,113 @@ close all;
 
 % speech: 5ms, 
 
-<<<<<<< Updated upstream
+%% double check by LPC-matlab
+% [signal, fs] = audioread("piano.wav");
+% 
+% M = floor(5e-3*fs);
+% 
+% num_segment = ceil(length(signal)/M);
+% num_pad = num_segment* M -length(signal);
+% paddedSignal = padarray(signal,[num_pad 0],0,'post');
+% s = reshape(paddedSignal,M,num_segment)';
+% s_fft = fft(paddedSignal);
+% 
+% pred = zeros(size(s));
+% err = zeros(size(s));
+% 
+% for ss = 1:num_segment
+%     [pred(ss,:) err(ss,:)] = lpc(s(ss,:), 219);
+% end
+% 
+% 
+% H = zeros(size(s));
+% 
+% for ss = 1:num_segment
+%     pred_index = freqz(1, pred(ss,:),220);
+%     H_current = fftshift(pred_index);
+%     H(ss,:) = H_current';
+% end
+% 
+% A = 1./H;
+% 
+% A_reshape = reshape(A',[848980 1]);
+% H_reshape = reshape(H',[848980 1]);
+% 
+% error_freq = A_reshape .* s_fft;
+% error_time = ifft(error_freq, fs);
+
+
+
+%%%%%%% chatgpt
+% % Obtain LPC coefficients
+% LPC_order = 12;
+% LPC_coeffs = lpc(x, LPC_order);
+% 
+% % Obtain the prediction filter
+% prediction_filter = dfilt.df1(LPC_coeffs);
+% 
+% % Filter the original signal using the prediction filter
+% residual = filter(prediction_filter.Numerator, 1, x);
+% 
+% % Obtain the LPC synthesis filter
+% synth_coeffs = [1, -LPC_coeffs(2:end)];
+% synth_filter = dfilt.df2sos(synth_coeffs);
+% 
+% % Synthesize the LPC residual using the LPC synthesis filter
+% predicted = filter(synth_filter.Numerator, synth_filter.Denominator, residual);
+% 
+% 
+
+
+% sound(abs(error_time))
+% audioOut =zeros(size(s));
+% for ss = 1:num_segment
+%     audioOut(ss,:) = filter([0 -pred(ss,2:end)], 1, s(ss,:));
+% end
+% 
+% audioOut_reshape = reshape(audioOut',[848980 1]);
+% 
+% sound(audioOut_reshape, 44100); 
+
+
 %%
 % [hPiano, ePiano] = LPCFilter("piano.wav");
 % [hSpeech, eSpeech] = LPCFilter("speech.wav");
-[predict_piano] = LPCFilter("piano.wav");
-predict_piano_reshape = reshape(predict_piano', 1, size(predict_piano,1)*size(predict_piano,2));
-sound(predict_piano_reshape)
-=======
-% Import the files
-[signal, fs] = audioread("piano.wav");
+[Hpiano, epiano] = LPCFilter("piano.wav");
+[Hspeech, espeech] = LPCFilter("speech.wav");
 
-% 5 ms is taken from lesson as example segment length
-M = floor(5e-3*fs); % How many samples in each segment
+error_time_piano = ifft(epiano);
+sound(abs(error_time_piano),44100)
+error_time_speech = ifft(espeech);
+
+sound(abs(error_time_speech),44100)
 
 
-%%%%//// Method by Xinmeng
-num_segment = ceil(length(signal)/M);
-num_pad = num_segment* M -length(signal);
-paddedSignal = padarray(signal,[num_pad 0],0,'post');
-s = reshape(paddedSignal,M,num_segment)';
+
+talking_freq = Hspeech .* epiano;
+talking_time = ifft(talking_freq);
+sound(abs(talking_time),44100)
+
+
+% a_exp1 = ones(size(a,1),1);
+% a_exp =[a_exp1 -1.*a];
 % 
-pred =zeros(size(s));
-err =zeros(size(s));
+% 
+% H = fftshift(freqz(1, a_exp));
 
-for ss = 1:num_segment
-    [pred(ss,:) err(ss,:)] = lpc(s(ss,:), 219);
-end
-
-audioOut =zeros(size(s));
-for ss = 1:num_segment
-    audioOut(ss,:) = filter([0 -pred(ss,2:end)], 1, s(ss,:));
-end
-
-audioOut_reshape = reshape(audioOut',[848980 1]);
-
-% sound(audioOut_reshape, 44100); 
->>>>>>> Stashed changes
-%%
-% [hSpeech, eSpeech] = LPCFilter("speech.wav");
-% [a] = LPCFilter("piano.wav");
-
-load a.mat;
-
-a_exp1 = ones(size(a,1),1);
-a_exp =[a_exp1 -1.*a];
-
-
-
-H = zeros(size(a_exp));
-for ii = 1:size(a_exp,1)
-    H(ii,:) = freqz(1, a_exp(ii,:), 220);
-end
-A = 1./H;
-
-a_exp0 = zeros(size(a,1),1);
-a_exp_p =[a_exp0 a];
-
-P = zeros(size(a_exp_p));
-for ii = 1:size(a_exp_p,1)
-    P(ii,:) = freqz(a_exp_p(ii,:),1,220);
-end
-
-
-%%%%%%%%
-
-% Import the files
-[signal, fs] = audioread("piano.wav");
-
-% 5 ms is taken from lesson as example segment length
-M = floor(5e-3*fs); % How many samples in each segment
-
-%%%%//// Method by Xinmeng
-num_segment = ceil(length(signal)/M);
-num_pad = num_segment* M -length(signal);
-paddedSignal = padarray(signal,[num_pad 0],0,'post');
-s = reshape(paddedSignal,M,num_segment)';
-
-% piano_fft = zeros(size(s));
-% for ss = 1:num_segment
-% piano_fft(ss,:)= fft(s(ss,:));
+% H = zeros(size(a_exp));
+% for ii = 1:size(a_exp,1)
+%     H(ii,:) = freqz(1, a_exp(ii,:), 220);
+% end
+% A = 1./H;
+% 
+% a_exp0 = zeros(size(a,1),1);
+% a_exp_p =[a_exp0 a];
+% 
+% P = zeros(size(a_exp_p));
+% for ii = 1:size(a_exp_p,1)
+%     P(ii,:) = freqz(a_exp_p(ii,:),1,220);
 % end
 
-piano_fft = fft(s');
-piano_fft = piano_fft';
 
-error = A.*piano_fft;
-error_reshape = reshape(error',[848980 1]);
-error_time = ifft(error_reshape')';
-
-piano_hat = P .* piano_fft;
-piano_hat_reshape = reshape(piano_hat',[848980 1]);
-
-
-  sound(abs(error_time))
-% sound(abs(piano_hat_reshape))
-sound(abs(paddedSignal-error_reshape))
-% H_reshape = reshape(H',[848980 1]);
-% piano_pad_zero = zeros(numel(a_exp)-length(signal));
-% piano_pad = [signal piano_pad_zero];
-% piano_fft = fft(piano_pad);
 
--- Homework 1/LPCFilter.m
+++ Homework 1/LPCFilter.m
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-function[s_predict] = LPCFilter (audioFile)
-=======
-function[a] = LPCFilter (audioFile)
->>>>>>> Stashed changes
+function[H_reshape, error_freq] = LPCFilter (audioFile)
 
 % Import the files
 [signal, fs] = audioread(audioFile);
@@ -10,12 +6,47 @@ function[a] = LPCFilter (audioFile)
 % 5 ms is taken from lesson as example segment length
 M = floor(5e-3*fs); % How many samples in each segment
 
-
 %%%%//// Method by Xinmeng
 num_segment = ceil(length(signal)/M);
 num_pad = num_segment* M -length(signal);
 paddedSignal = padarray(signal,[num_pad 0],0,'post');
 s = reshape(paddedSignal,M,num_segment)';
+s_fft = fft(paddedSignal);
+%%%%%////Method by Marco 
+%  index = 1:M:length(signal); %resize the piano signal
+% s = zeros(length(index), M); %piano signal split in segments
+% 
+% % Create a new signal by appending zeros at the end s.t. length(paddedSignal) is
+% % multiple of M
+% paddedSignal = zeros(numel(s),1);
+% paddedSignal(1:length(signal)) = signal(:);
+% 
+% % split the signal in segments of length M
+% for ii = 1:length(s)
+%     s(ii,:) = paddedSignal(index(ii) : index(ii)+M-1);
+% end
+
+
+% 
+% % Create the r vector of the autocorrelations with sample lag 1:M
+% r = zeros(length(s), size(s,2)); % n , p
+% for nn = 1:length(s)
+%     for pp = 1:M
+%         r(nn,pp) = sum(s(nn,1:M-pp).*s(nn,1+pp:M));
+%     end
+% end
+% 
+% 
+% % Create autocorrelation vector with sample lags 0:M-1
+% acorrVector = zeros(length(s), size(s,2));
+% for nn = 1:length(s)
+%     for pp = 0:M-1
+%         acorrVector(nn,pp+1) = sum(s(nn,1:M-pp).*s(nn,1+pp:M));
+%     end
+% end
+
+
+
 
 %%%%%//// auto-correlation matrix by xinmeng
 
@@ -55,93 +86,38 @@ for ss= 1:num_segment
 end
 
 r = r';
-a = a'; % a vector
-r_auto_correlation = r_auto_correlation';
-<<<<<<< Updated upstream
-% R = permute(R, [2 1 3]);
-
-
-%%%%//////////////////////////////////
-
-
-%%
-% Create a predicted version of the file by convolving the filter with the signal
-=======
->>>>>>> Stashed changes
-
-
+a = a';
 a_exp1 = ones(size(a,1),1);
 a_exp =[a_exp1 -1.*a];
-H = zeros(size(a_exp)); % shaping filter H
-for ii = 1:size(a_exp,1)
-    H(ii,:) = freqz(1, a_exp(ii,:), M);
-end
-A = 1./H; % whitening filter A
 
-<<<<<<< Updated upstream
-ww=1;
+%%% get filter
 
-
-%%%%/// predicted version by Marco
-% sPredict = zeros(size(s));
-% for nn = 1:size(s,1)
-%     for kk = 1:size(s,2)
-%         sPredict(nn,kk) = sum(a(nn,1:kk) .* s(nn, kk:-1:1));
-%     end
-% end
-% 
-% predictedSignal = zeros(size(paddedSignal));
-% for ii = 1:size(index,2)-1
-%     predictedSignal(index(ii):index(ii+1)-1) = sPredict(ii,:);
-% end
-
-
-%%%%%%%%%%%%%%%
-% % Z-Transform of the predicted signal by Xinmeng
-% 
-% sPredict_fft = zeros(size(s_predict)); % 用于存储每行 FFT 的结果
-% 
-% for i = 1:num_segment
-%     sPredict_fft(i, :) = fft(s_predict(i, :));
-% end
-% 
-% % Z-Transoform of the predicted signal by Marco
-% sPredictZ = fft(predictedSignal);
-% N = length(sPredictZ);
-% z = exp(2*pi*1i/N);
-% k = 0:N-1;
-% sPredictZ = ((1/N) * sPredictZ' .* z.^(-k))';
-% 
-% %Z-Transform of the original signal
-% sZ = fft(paddedSignal);
-% sZ = ((1/N) * sZ' .* z.^(-k))';
-% 
-% %Find Z-Transform of the error
-% error = sPredictZ - sZ;
-% 
-% % Find filter H
-% H = (sPredictZ ./error);
-=======
-a_exp0 = zeros(size(a,1),1);
-a_exp_p =[a_exp0 a];
-
-P = zeros(size(a_exp_p));
-for ii = 1:size(a_exp_p,1)
-    P(ii,:) = freqz(a_exp_p(ii,:),1,M);
+%%%%%%%%%%%% method 1 - SHIT！ 
+H = zeros(size(s));
+for ss = 1:num_segment
+    pred_index = freqz(1, a_exp(ss,:),220);
+    H(ss,:) = pred_index';
+%     H_current = fftshift(pred_index);
+%     H(ss,:) = H_current';
 end
 
+A = 1./H;
 
-piano_fft = fft(s');
-piano_fft = piano_fft';
+A_reshape = reshape(A',[848980 1]);
+H_reshape = reshape(H',[848980 1]);
 
-error = A.*piano_fft;
-error_reshape = reshape(error',[848980 1]);
-error_time = ifft(error_reshape')';
-
-piano_hat = P .* piano_fft;
-piano_hat_reshape = reshape(piano_hat',[848980 1]);
+error_freq = A_reshape .* s_fft;
+error_time = ifft(error_freq);
 
 
+% H = fftshift(freqz(1, a_exp));
 
-
->>>>>>> Stashed changes
+% %%%% transfer frequency
+% 
+% audioOut =zeros(size(s));
+% for ss = 1:num_segment
+%     audioOut(ss,:) = filter([0 -a_exp(ss,2:end)], 1, s(ss,:));
+% end
+% 
+% audioOut_reshape = reshape(audioOut',[num_segment 1]);
+% 
